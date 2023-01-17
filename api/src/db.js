b91703cfaed04ea2840c06f5df4b7e0d52b3d808
EsteChar2906 <ccarlospachecoc@gmail.com>
2023-01-17 15:28:42 -0500

    arreglando deploy

--- api/src/db.js
+++ api/src/db.js
@@ -4,13 +4,17 @@ const { db } = require('./config.js');
 
 const pool = mysql.createConnection(db);
 
-pool.connect((err) => {
-	if(err){
-		console.log(err)
-	}
-	console.log('DB is Connected');
-	return;
-});
+const connection = async() =>{
+	await pool.connect((err) => {
+		if(err){
+			throw err;
+		}
+		console.log('DB is Connected');
+		return;
+	})
+};
+
+connection();
 
 //promisify para querys
 pool.query = promisify(pool.query);
