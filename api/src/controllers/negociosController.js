faca0c447cb9fa1e3d003fc5cc652d2f2301e493
EsteChar2906 <ccarlospachecoc@gmail.com>
2023-01-17 18:37:58 -0500

    arreglando llamado a la db

--- api/src/controllers/negociosController.js
+++ api/src/controllers/negociosController.js
@@ -13,25 +13,25 @@ const controller = {
 			User_Id: req.user.ID
 		};
 
-		await pool.query('INSERT INTO negocios set ?', [negocioNuevo]);
+		await pool.query('INSERT INTO Negocios set ?', [negocioNuevo]);
 		
 		req.flash('Realizado', 'Guardado correctamente');
 		res.redirect('/api/listnegocios');
 	},
 	listNegocios: async(req, res) => {
 		const IdUser = req.user.ID;
-		const rows = await pool.query('SELECT * FROM negocios WHERE User_id = ?', [IdUser]);
+		const rows = await pool.query('SELECT * FROM Negocios WHERE User_id = ?', [IdUser]);
 		res.render('negocios/listNegocios.hbs', {negocios: rows})
 	},
 	deleteNegocio: async(req, res) => {
 		let { id } = req.params;
 
-		await pool.query('DELETE FROM negocios WHERE ID = ?', [id]);
+		await pool.query('DELETE FROM Negocios WHERE ID = ?', [id]);
 		res.redirect('/api/listnegocios')
 	},
 	editarNegocio: async(req, res) => {
 		let { id } = req.params;
-		const rows = await pool.query('SELECT * FROM negocios WHERE ID = ?', [id])
+		const rows = await pool.query('SELECT * FROM Negocios WHERE ID = ?', [id])
 		res.render('negocios/editForm.hbs', {negocio: rows[0]});
 	},
 	editUpdNegocio: async(req, res) => {
@@ -48,7 +48,7 @@ const controller = {
 		await pool.query('UPDATE negocios set ? WHERE ID = ?', [negocioNuevo, id])
 	},
 	listNegociosAllUser: async(req, res) => {
-		const negociosUser = await pool.query('SELECT * FROM negocios, usuarios WHERE negocios.User_id = usuarios.ID');
+		const negociosUser = await pool.query('SELECT * FROM Negocios, usuarios WHERE Negocios.User_id = usuarios.ID');
 		res.render('negocios/allUsersNegocios.hbs', {negocios: negociosUser});
 		
 	}
