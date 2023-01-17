c460a5d18b772853193e625b46d079ddcfef150d
Carlos Pacheco Pedraza <74948016+EsteChar2906@users.noreply.github.com>
2023-01-17 17:16:55 -0500

    Update config.js

--- api/src/config.js
+++ api/src/config.js
@@ -6,7 +6,10 @@ module.exports = {
 		user: process.env.USER_DB,
 		password: process.env.PASSWORD_DB,
 		port: process.env.PORT_DB,
-		database: process.env.NAME_DB
+		database: process.env.NAME_DB,
+		waitForConnections: true,
+		connectionLimit: 10,
+		queueLimit: 0
 	},
 	appS: {
 		PORT: process.env.PORT
